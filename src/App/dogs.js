6f100073abfcbfb37ab0507c70af7cd26d04da33
Silvestre Luna <slvtrluna@gmail.com>
2019-06-20 18:18:41 -0500

    created dogs data

---
+++ src/App/dogs.js
@@ -0,0 +1,39 @@
+const dogs = [
+  {
+    id: 'dog1',
+    name: 'Apollo',
+    age: '1 Year',
+    type: 'Bulldog',
+    color: 'Brown',
+  },
+  {
+    id: 'dog2',
+    name: 'Zeus',
+    age: '2 Years',
+    type: 'German Shepherd',
+    color: 'Black',
+  },
+  {
+    id: 'dog3',
+    name: 'Orion',
+    age: '3 Years',
+    type: 'Poodle',
+    color: 'White',
+  },
+  {
+    id: 'dog4',
+    name: 'Neo',
+    age: '4 years',
+    type: 'Pug',
+    color: 'Buff',
+  },
+  {
+    id: 'gog5',
+    name: 'Alba',
+    age: '5 Years',
+    type: 'Boxer',
+    color: 'Orange',
+  },
+];
+
+export default dogs;
