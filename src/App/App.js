3a94495cc6a83875cb8cacf362f8d3d6b1632aef
Silvestre Luna <slvtrluna@gmail.com>
2019-06-22 10:30:10 -0500

    completed shape

--- src/App/App.js
+++ src/App/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
     dogs: [],
     employees: [],
   }
-
+  
   componentDidMount() {
     this.setState({ dogs: myDogs });
     this.setState({ employees: allEmployees });
