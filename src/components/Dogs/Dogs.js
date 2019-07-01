73336f8b479fbfe7776a625378422a61429de9b3
Silvestre Luna <slvtrluna@gmail.com>
2019-07-01 14:43:23 -0500

    created seed data in firebase and made axios call

--- src/components/Dogs/Dogs.js
+++ src/components/Dogs/Dogs.js
@@ -5,13 +5,17 @@ import './Dogs.scss';
 class Dogs extends React.Component {
   render() {
     const { dog } = this.props;
+
     return (
-      <div className="Dogs col-3 card m-1">
+      <div className="Dogs col-3 card m-2">
         <div className="card-body">
           <img src={dog.imgUrl} alt="dog" />
           <h3 className="card-title">{dog.name}</h3>
           <p className="card-text">{dog.type}, {dog.color}</p>
           <p className="card-text">{dog.age}</p>
+          <div className="walker text-muted">
+            <h6>Walker: <span>John</span></h6>
+          </div>
         </div>
       </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,12 +5,16 @@ import DogPen from '../DogPen/DogPen';
 import StaffRoom from '../StaffRoom/StaffRoom';
 import dogsData from '../../helpers/data/getDogsData';
 import employeesData from '../../helpers/data/getEmployeesData';
+import allWalks from '../../helpers/data/getWalkersData';
+
+import './Home.scss';
 
 
 class Home extends React.Component {
   state = {
     dogs: [],
     employees: [],
+    walks: [],
   }
 
   getAllData = () => {
@@ -21,6 +25,10 @@ class Home extends React.Component {
     employeesData.getAllEmp()
       .then(employees => this.setState({ employees }))
       .catch(err => console.error(err, 'No emp data came back'));
+
+    allWalks.getWalksData()
+      .then(walks => this.setState({ walks }))
+      .catch(err => console.error(err, 'No walks came back'));
   }
 
   componentDidMount() {
@@ -30,12 +38,14 @@ class Home extends React.Component {
   render() {
     const { dogs } = this.state;
     const { employees } = this.state;
+    const { walks } = this.state;
+
     return (
       <div className="Home">
         <h1>Doggie Day Care</h1>
-        <DogPen dogs ={dogs}/>
+        <DogPen dogs={dogs} walks={walks} />
         <h5 className="m-3">Employees</h5>
-        <StaffRoom employees={employees}/>
+        <StaffRoom employees={employees} />
       </div>
     );
   }
